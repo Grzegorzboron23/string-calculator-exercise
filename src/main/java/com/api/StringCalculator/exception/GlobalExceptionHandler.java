b60b44ae50583b74952062a808fb7c2a7863dd01
Grzegorz Boroń <grzegorzboron23@gmail.com>
2025-07-26 11:12:44 +0200

    feat: 6 add very simple logger

--- src/main/java/com/api/StringCalculator/exception/GlobalExceptionHandler.java
+++ src/main/java/com/api/StringCalculator/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.api.StringCalculator.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,11 +8,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.ZonedDateTime;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(StringCalculatorException.class)
     public ResponseEntity<ApiExceptionResponse> handleStringCalculatorException(StringCalculatorException ex) {
+        log.warn("Handled StringCalculatorException: {}", ex.format(), ex);
+
         ApiExceptionResponse response = new ApiExceptionResponse(
                 "Validation error",
                 ex.format(),
@@ -23,6 +27,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiExceptionResponse> handleGenericException(Exception ex) {
+        log.error("Unhandled exception occurred", ex);
+
         ApiExceptionResponse response = new ApiExceptionResponse(
                 "Unexpected error occurred",
                 ex.getMessage(),
@@ -32,3 +38,4 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
+
